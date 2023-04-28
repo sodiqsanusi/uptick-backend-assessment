8cdfa0715f8764c081c25ac4abd6fa68eb9baccf
Sodiq Sanusi <ademilddiq@gmail.com>
2023-04-28 01:35:57 +0100

    Added up an error handler middleware

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const CONFIG = require("./config/env");
 const express = require("express");
 const connectDB = require("./config/db");
+const errorHandler = require("./middleware/errorMiddleware");
 
 connectDB()
 const app = express();
 
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/notes", require("./routes/noteRoutes"));
 // app.use("/api/user", require("./routes/userRoutes"));
 
 
+app.use(errorHandler);
 
 app.listen(CONFIG.PORT, () => {
   console.log(`Server is running on port ${CONFIG.PORT}`)
---
+++ middleware/errorMiddleware.js
@@ -0,0 +1,16 @@
+const { NODE_ENV } = require("../config/env");
+
+let errorMiddleware = (err, req, res, next) => {
+
+  let errStatus = err.statusCode || 500;
+  let errMessage = err.message || "An error occured while making this request";
+
+  res.status(errStatus).json({
+    success: false,
+    status: errStatus,
+    message: errMessage,
+    stack: NODE_ENV === "production" ? null : err.stack,
+  })
+};
+
+module.exports = errorMiddleware
